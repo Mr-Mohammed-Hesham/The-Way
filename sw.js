10a56123c1d83d6e485404475bbba0f79fbcb4a9
Mr-Mohammed-Hesham <mr.mohamed.hesham93@gmail.com>
2026-09-04 22:29:30 +0400

    Updates

---
+++ sw.js
@@ -0,0 +1,15 @@
+// The Way Training Center - Service Worker (4U PWA Architecture)
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
