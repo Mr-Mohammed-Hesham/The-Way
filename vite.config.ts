cdcc267b0843c01ec986bd9f9efaf9dfee3b4162
Mr-Mohammed-Hesham <mr.mohamed.hesham93@gmail.com>
2026-09-04 22:25:56 +0400

    Fix GitHub Pages paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,19 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/The-Way/',
+
   plugins: [
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     host: '0.0.0.0',
     port: 3000,
